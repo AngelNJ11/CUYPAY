673c6983dccfa85974dca3a511acceaf945f4c03
KAWORU <angelnj733@gmail.com>
2025-05-03 03:26:21 -0500

    dise;o y controller de ingreso

--- CUYPAY/viewController/GestionViewController.swift
+++ CUYPAY/viewController/GestionViewController.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class GestionViewController: UIViewController {
 
+    @IBOutlet weak var bntTipo: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func seleccionarTipo(_ sender:UIAction ){
+        print(sender.title)
+        self.bntTipo.setTitle(sender.title, for: .normal)
     }
-    */
-
+    
+    @IBAction func volver(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
