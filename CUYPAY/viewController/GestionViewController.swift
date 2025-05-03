6801217d8c96bf2a3da8a06bd81f180f51a95277
KAWORU <angelnj733@gmail.com>
2025-05-03 03:25:23 -0500

    Ingreso

---
+++ CUYPAY/viewController/GestionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GestionViewController.swift
+//  CUYPAY
+//
+//  Created by KAWORU on 3/05/25.
+//
+
+import UIKit
+
+class GestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
